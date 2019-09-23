ea5225a2d92db4c5c0040d1c94fcfe59bc08d63e
merlinzjl <merlinzjl@163.com>
2019-09-23 22:58:31 +0800

    gray and threshold

---
+++ 01gray&threshold/gray/gray.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	/*读灰度图像*/
+	Mat mat = imread("..//..//pictures//01flower.jfif", IMREAD_GRAYSCALE);
+	/*显示灰度图像*/
+	imshow("flower", mat);
+
+	/*二值化*/
+	Mat thresholdResult;
+	threshold(mat, thresholdResult, 100, 255, ThresholdTypes::THRESH_BINARY);
+	imshow("threshold", thresholdResult);
+	
+	waitKey();
+
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Opencv
 Opencv函数使用实例
 
-环境：Vs2017 + opencv4.1.0
+环境：win10+Vs2017 + opencv4.1.0
+
+Opencv 官方API文档
+https://docs.opencv.org/
